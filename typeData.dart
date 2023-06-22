187dd804731d78a65009d79fcb9e1cef1924f1f1
Heri Irawan <81469614+Heri-Irawan@users.noreply.github.com>
2023-06-22 16:08:43 +0800

    menambahkan type data

--- typeData.dart
+++ typeData.dart
@@ -1,4 +1,5 @@
 void main() {
+  // number
   int nomor = -10;
   double harga = 10.0;
   // properti pada number
@@ -9,4 +10,22 @@ void main() {
   print(nomor.isOdd);
   print(harga.isNaN);
   print(nomor.sign);
+
+// String
+  String nama = "heri";
+  String alamat = """jln. saleh sungkar nomer 2, nusa tenggara timur
+  """;
+  print("nama saya $nama dan alamat saya $alamat");
+
+  // boolean
+  bool isTrue = 5 > 15;
+  print(isTrue);
+
+  // if else menggunakan boolean
+  int angkaValid = 100;
+  if (angkaValid == 100) {
+    print("angka anda valid");
+  } else {
+    print("angka anda tidak valid");
+  }
 }
