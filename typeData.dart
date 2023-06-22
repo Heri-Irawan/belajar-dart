d0f608f7ffdf2f9099d86b4c8ac552f4d616333a
Heri Irawan <81469614+Heri-Irawan@users.noreply.github.com>
2023-06-22 15:57:35 +0800

    first commit

---
+++ typeData.dart
@@ -0,0 +1,12 @@
+void main() {
+  int nomor = -10;
+  double harga = 10.0;
+  // properti pada number
+  print(harga.hashCode);
+  print(harga.isFinite);
+  print(harga.isInfinite);
+  print(nomor.isNegative);
+  print(nomor.isOdd);
+  print(harga.isNaN);
+  print(nomor.sign);
+}
