67edbec02680762b8e9448a82b8ebb8dbd2cd793
Heri Irawan <81469614+Heri-Irawan@users.noreply.github.com>
2023-06-22 16:23:26 +0800

    operator

---
+++ operator.dart
@@ -0,0 +1,18 @@
+void main() {
+  // operator aritmatika ( + - * / %)
+  // incrmenet dan decrement
+  var nilai = 1;
+  print(++nilai);
+
+  // perbandingan ( >, <, >=, <=, ==, !=)
+  var harga = 10;
+  var menawar = 5;
+
+  if (harga == menawar) {
+    print("terlalu murah");
+  } else if (harga < menawar) {
+    print("masih terlalu murah");
+  } else if (harga > menawar) {
+    print("selamat anda bisa membeli");
+  }
+}
